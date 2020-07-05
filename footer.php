1f0b519b4e99987a73d4c33e07721bc642506cc3
Alvaro Hilado <jose.alvaro.hilado@gmail.com>
2020-07-05 21:21:49 +0800

    Footer update

--- footer.php
+++ footer.php
@@ -10,8 +10,12 @@
               if(have_rows ('ways_to_give_repeater','option')):
                 while(have_rows('ways_to_give_repeater','option')) : the_row();
                 ?>
-                <li><a href="<?php the_sub_field('link_url');?>"><?php the_sub_field('link_text');?></a></li>
+                <?php if(get_sub_field('link_url') != NULL || get_sub_field('link_url') != ''):?>
+                  <li><a href="<?php the_sub_field('link_url');?>"><?php the_sub_field('link_text');?></a></li>
+                <?php else :?>
+                  <li><?php the_sub_field('link_text');?></li>
                 <?php
+                  endif;
                 endwhile;
               endif;
               ?>
@@ -24,8 +28,12 @@
                 if(have_rows ('more_from_sasai_repeater','option')):
                   while(have_rows('more_from_sasai_repeater','option')) : the_row();
                   ?>
-                  <li><a href="<?php the_sub_field('link_url');?>"><?php the_sub_field('link_text');?></a></li>
+                  <?php if(get_sub_field('link_url') != NULL || get_sub_field('link_url') != ''):?>
+                    <li><a href="<?php the_sub_field('link_url');?>"><?php the_sub_field('link_text');?></a></li>
+                  <?php else :?>
+                    <li><?php the_sub_field('link_text');?></li>
                   <?php
+                    endif;
                   endwhile;
                 endif;
               ?>
@@ -34,10 +42,15 @@
           <div class="col-md-3 col-lg-3">
             <h6>Contact Us</h6>
             <ul class="list-unstyled">
-              <li><?php the_field('phone_number','option')?></li>
-              <li><?php the_field('address','option')?></li>
-              <li><?php the_field('australia_charity_number','option')?></li>
-              <li><?php the_field('copyright','option')?></li>
+                <?php
+                  if(have_rows('contact_details','option')): 
+                    while(have_rows('contact_details','option')) : the_row() 
+                ?>
+                  <li><?php the_sub_field('contact_text')?></li>
+                <?php
+                    endwhile;
+                  endif;
+                ?>
             </ul>
           </div>
           <div class="col-md-3 col-lg-3 text-center">
@@ -47,6 +60,9 @@
               <a href="<?php the_field('youtube_url','option')?>" type="button" class="btn" role="button"><i class="fab fa-youtube"></i></a>
               <a href="<?php the_field('twitter_url','option')?>" type="button" class="btn" role="button"><i class="fab fa-twitter"></i></a>
               <a href="<?php the_field('linked_in_url','option')?>" type="button" class="btn" role="button"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div>
+                <?php the_field('bank_details','option')?>
             </div>
           </div>
         </div>
